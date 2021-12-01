7a8260a495dabc707791332bcd00eac7d6176ccb
Vagif Molla <vagif69@gmail.com>
2021-12-01 17:49:12 +0200

    Complete main app file

--- app.py
+++ app.py
@@ -6,7 +6,7 @@ from flask import Flask
 app = Flask(__name__)
 cache = redis.Redis(host='redis', port=6379)
 
-def get_hit_count():
+def get_hit_count() -> None:
     retries = 5
     while True:
         try:
@@ -17,3 +17,7 @@ def get_hit_count():
             retries -= 1
             time.sleep(0.5)
 
+@app.route('/')
+def hello() -> str:
+    count = get_hit_count()
+    return 'Hello World! I have been seen {} times.\n'.format(count)
